7b1d366f24a1976be8b9f43b4e7563dcc8efddd5
masudrana <masudranaacb@gmail.com>
2022-01-31 23:29:31 +0600

    using randome number creation

--- usingmath.js
+++ usingmath.js
@@ -1,3 +1,6 @@
 const myNumber=5.778;
-let result=Math.ceil(myNumber);
-console.log(result);
+for(let i=0; i<=100; i++){
+    let result=Math.random()*10;
+let rounded=Math.round(result);
+console.log(rounded);
+}
