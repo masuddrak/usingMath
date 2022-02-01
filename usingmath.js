bdf331ac0013d76bf0a14574317113d231c115a7
masudrana <masudranaacb@gmail.com>
2022-02-01 14:29:41 +0600

    thre find lowes number

--- usingmath.js
+++ usingmath.js
@@ -1,14 +1,14 @@
-// add a three number find the biggest number
+// add a three number find the lowest number
 function findLargest(frist,second,third){
-    if(frist>second && frist>third){
-        return frist;
-    }
-    else if(second>frist && second>third){
-        return second;
-    }
-    else{
-        return third;
-    }
+   if(frist<second && frist<third){
+       return frist;
+   }
+   else if(second<frist && second<third){
+       return second;
+   }
+   else{
+       return third;
+   }
 }
-var result=findLargest(21,45,2);
-console.log("thats is largest number",result);
+var result=findLargest(3,2,1);
+console.log("thats is lowes number",result);
